ce6ed0cec9b8c6e9daede257a09cb8c38de29bdd
Ismat Ismoilov <ismatismoilov709@gmail.com>
2026-05-14 16:07:26 +0500

    fix: next config and api url

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "16.170.235.75",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
